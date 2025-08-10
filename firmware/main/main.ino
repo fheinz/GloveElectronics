8f4f1af50621b30eb6ba15a1fed20d87c95061e9
Fede <fheinz@gmail.com>
2025-08-10 10:33:00 +0100

    Code cleanup
    Uncut some corners I had cut during quick iteration. This doesn't change any functionality.

--- firmware/main/main.ino
+++ firmware/main/main.ino
@@ -140,12 +140,15 @@ constexpr uint8_t vibration_patterns[] = {
   VIBRATION_PATTERN(3, 2, 0, 1),
   VIBRATION_PATTERN(3, 2, 1, 0),
 };
-constexpr unsigned int num_vibration_patterns = sizeof(vibration_patterns) / sizeof(vibration_patterns[0]);
+constexpr unsigned int NUM_VIBRATION_PATTERNS = sizeof(vibration_patterns) / sizeof(vibration_patterns[0]);
 
 //**********************************************************************************
 // Motor control
 
 constexpr int TARGET_DRIVE_FREQUENCY = 250;  // 250 Hz according to Pfeifer et al. 2021
+constexpr int PULSE_DURATION = pdMS_TO_TICKS(100);
+constexpr int PAUSE_DURATION = pdMS_TO_TICKS(66);
+constexpr int ACTIVE_CYCLE_DURATION = 4 * (PULSE_DURATION + PAUSE_DURATION);
 
 // The chip expects a PWM signal where duty cycle 50 means stopped, and 255 is full speed in one phase, 0 full speed in the opposite phase.
 // Though for signal reasons we can't actually use 0% and 100%.
@@ -173,7 +176,7 @@ void RunMotors(int pattern, bool is_client) {
   Serial.printf("%010u Running sequence %02d\n", xTaskGetTickCount(), pattern);
 
   if (pattern < 0) {
-    vTaskDelay(pdMS_TO_TICKS(664));
+    vTaskDelay(pdMS_TO_TICKS(ACTIVE_CYCLE_DURATION));
     return;
   }
 
@@ -189,7 +192,7 @@ void RunMotors(int pattern, bool is_client) {
     int finger = seq & 0x03;
     ledcWrite(PWM_PINS[finger], ACTIVE_DUTY_CYCLE);
     digitalWrite(LED_PIN, HIGH);
-    vTaskDelay(pdMS_TO_TICKS(100));
+    vTaskDelay(PULSE_DURATION);
     ledcWrite(PWM_PINS[finger], 128);
     digitalWrite(LED_PIN, LOW);
     // Doing the skew computation here assumes that the skew is < 100ms, which should be safe
@@ -200,7 +203,7 @@ void RunMotors(int pattern, bool is_client) {
         Serial.printf("%010u Skew %dms\n", xTaskGetTickCount(), start_time - sync_time);
       }
     }
-    vTaskDelay(pdMS_TO_TICKS(66));
+    vTaskDelay(PAUSE_DURATION);
   }
   Serial.printf("%010u Done sequence %02d\n", xTaskGetTickCount(), pattern);
 }
@@ -216,6 +219,12 @@ constexpr char* SERVICE_UUID = "21014a2d-ebee-4fcb-b8d4-dcf34c19610a";
 constexpr char* CHARACTERISTIC_UUID = "44f21c6b-b08b-4695-970f-f21a15b538db";
 
 constexpr int BLE_SCAN_TIME_S = 5;
+
+typedef struct {
+  TickType_t sender_timestamp;
+  TickType_t next_cycle_start;
+  int pattern;
+} SyncMessage;
 
 // For BLE server.
 BLEServer* server = nullptr;
@@ -237,6 +246,11 @@ class ServerCallbacks : public BLEServerCallbacks {
   }
 };
 
+void SetSyncMessage(TickType_t next_cycle, int pattern) {
+  SyncMessage message = { xTaskGetTickCount(), next_cycle, pattern };
+  characteristic->setValue((uint8_t*)&message, sizeof(message));
+}
+
 void StartBLEServer() {
   BLEDevice::init("Vibrating Glove (Server)");
   server = BLEDevice::createServer();
@@ -244,8 +258,7 @@ void StartBLEServer() {
   service = server->createService(SERVICE_UUID);
   characteristic = service->createCharacteristic(
     CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
-  uint32_t data[] = { xTaskGetTickCount(), next_motor_cycle_start, current_pattern };
-  characteristic->setValue((uint8_t*)data, sizeof(data));
+  SetSyncMessage(next_motor_cycle_start, current_pattern);
   service->start();
 
   advertising = BLEDevice::getAdvertising();
@@ -266,8 +279,7 @@ void BLEServerLoop() {
     advertising->start();
   }
   if (device_connected) {
-    uint32_t data[] = { xTaskGetTickCount(), next_motor_cycle_start, current_pattern };
-    characteristic->setValue((uint8_t*)data, sizeof(data));
+    SetSyncMessage(next_motor_cycle_start, current_pattern);
     characteristic->notify();
   }
   last_device_connected = device_connected;
@@ -292,17 +304,17 @@ class ClientCallbacks : public BLEClientCallbacks {
 static void ClientNotifyCallback(BLERemoteCharacteristic* rc, uint8_t* data, size_t len, bool is_notify) {
   static TickType_t last_cycle_start_received = 0;
   TickType_t client_ticks = xTaskGetTickCount();
-  uint32_t value[3];
+  SyncMessage message;
 
-  if (len != sizeof(value)) {
-    Serial.printf("Wrong data length: %u, expected %d", len, sizeof(value));
+  if (len != sizeof(message)) {
+    Serial.printf("Wrong data length: %u, expected %d", len, sizeof(message));
     return;
   }
 
-  memcpy(&value, data, len);
-  TickType_t server_ticks = value[0];
-  TickType_t next_cycle_start = value[1];
-  current_pattern = value[2];
+  memcpy(&message, data, len);
+  TickType_t server_ticks = message.sender_timestamp;
+  TickType_t next_cycle_start = message.next_cycle_start;
+  current_pattern = message.pattern;
 
   if (last_cycle_start_received != next_cycle_start) {
     int32_t tick_drift = (uint32_t)(client_ticks - server_ticks);
@@ -403,9 +415,9 @@ void ble_client_task(void* parameter) {
 }
 
 // Rounds of five 1-second cycles, of which the first three are active, and the last two are passive
-constexpr TickType_t pattern_frequency = pdMS_TO_TICKS(1000);
-constexpr unsigned int cycles_per_round = 5;
-constexpr unsigned int active_cycles_per_round = 3;
+constexpr TickType_t FULL_CYCLE_LENGTH = pdMS_TO_TICKS(1000);
+constexpr unsigned int CYCLES_PER_ROUND = 5;
+constexpr unsigned int ACTIVE_CYCLES_PER_ROUND = 3;
 
 void motor_server_task(void* parameter) {
   pinMode(SYNC_PIN, INPUT);
@@ -416,11 +428,11 @@ void motor_server_task(void* parameter) {
 
   while (true) {
     // Select a pattern. 
-    current_pattern = (cycle_number++ % cycles_per_round < active_cycles_per_round) ? random(num_vibration_patterns) : -1;
+    current_pattern = (cycle_number++ % CYCLES_PER_ROUND < ACTIVE_CYCLES_PER_ROUND) ? random(NUM_VIBRATION_PATTERNS) : -1;
     // Wait for the start of the new cycle. The BLE cycle runs on a higher frequency, so this wait should give it
     // plenty of oportunity to communicate the new pattern to the client in time for it to execute it in sync.
-    next_motor_cycle_start = last_wake_time + pattern_frequency;
-    vTaskDelayUntil(&last_wake_time, pattern_frequency);
+    next_motor_cycle_start = last_wake_time + FULL_CYCLE_LENGTH;
+    vTaskDelayUntil(&last_wake_time, FULL_CYCLE_LENGTH);
     // Execute the pattern, the client ought to have gotten the memo by this time and, depending on the accuracy of
     // our sync, be executing it around now too.
     RunMotors(current_pattern, false);
@@ -440,7 +452,7 @@ void motor_client_task(void* parameter) {
   vTaskDelayUntil(&last_wake_time, next_motor_cycle_start - last_wake_time);
   while (true) {
     RunMotors(current_pattern, true);
-    vTaskDelayUntil(&last_wake_time, pattern_frequency);
+    vTaskDelayUntil(&last_wake_time, FULL_CYCLE_LENGTH);
   }
 }
 
